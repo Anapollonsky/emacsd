b62993bf22b7dc3cb550519f4310f433580f6d33
Andrew Apollonsky <anapollonsky@gmail.com>
2015-01-08 07:58:25 -0500

    Tried to merge initialization.org..

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Work Proxy
-;; (setq url-proxy-services '(("no_proxy" . "work\\.com")
-;;                            ("http" . "global.proxy.lucent.com:8000")))
+ (setq url-proxy-services '(("no_proxy" . "work\\.com")
+                            ("http" . "global.proxy.lucent.com:8000")))
 
 ;; Package repositories
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
@@ -18,3 +18,16 @@
 
 ;; Proceed with rest of initialization
  (org-babel-load-file "~/.emacs.d/initialization.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-use-overlays nil)
+ '(org-agenda-files (quote ("~/org/notes.org"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
