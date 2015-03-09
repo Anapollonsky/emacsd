f59e2975452d0433ca2eb1fdae8f4f17708bfd56
Andrew Apollonsky <Anapollonsky@gmail.com>
2015-03-09 17:46:51 -0500

    Restructured to get rid of bind-key complaints.

--- init.el
+++ init.el
@@ -8,19 +8,6 @@
            finally (cl-return t)))
 
 
-(if (not (package-installed-p 'bind-key))
-    (progn
-      (package-refresh-contents)
-      (package-install 'use-package)))
-(require 'bind-key)
-
-(if (not (package-installed-p 'use-package))
-    (progn
-      (package-refresh-contents)
-      (package-install 'use-package)))
-(require 'use-package)
-
-
 ;; Work Proxy
 (when (not (can-retrieve-packages))
 (setq url-proxy-services '(("no_proxy" . "work\\.com")
@@ -30,13 +17,10 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("melpa" . "http://melpa.milkbox.net/packages/")))
 
-;; Single package to auto-install package if not present. Only used to bootstrap use-package.
-
 (setq package-enable-at-startup nil)
 (package-initialize)
 
-
-;; Automatically install use-package
+;; Single package to auto-install package if not present. Only used to bootstrap use-package.
 (or (package-installed-p 'use-package) (package-install 'use-package))
 (require 'use-package)
 
